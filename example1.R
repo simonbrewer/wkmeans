00e86f8cb107925e7a9193905e37e491996835e3
Simon Brewer <brewersi@gmail.com>
2023-05-23 11:02:21 -0600

    Updated example 1 with weights

--- example1.R
+++ example1.R
@@ -7,4 +7,23 @@ iris2 <- iris %>%
 
 head(iris2)
 
-wkmeans(data = iris2, k = 3)
+iris.wkmeans <- wkmeans(data = iris2, k = 3)
+
+plot(iris$Petal.Length, iris$Petal.Width, 
+     pch =16, col = iris.wkmeans$clusters)
+points(iris.wkmeans$centers$Petal.Length, 
+       iris.wkmeans$centers$Petal.Width,
+       pch = 10, cex = 2, col = 1:3)
+
+wgts <- rep(1, nrow(iris))
+wgts[which.min(iris$Petal.Length)] <- 1e5
+wgts[which.max(iris$Petal.Length)] <- 1e5
+
+iris.wkmeans <- wkmeans(data = iris2, k = 3, w = wgts)
+
+plot(iris$Petal.Length, iris$Petal.Width, 
+     pch =16, col = iris.wkmeans$clusters)
+points(iris.wkmeans$centers$Petal.Length, 
+       iris.wkmeans$centers$Petal.Width,
+       pch = 10, cex = 2, col = 1:3)
+
